c3226bc8c3fbed9dfb5e330845b63ebdef28f577
Faisal Nur <102789269+Faisalkn@users.noreply.github.com>
2024-04-27 19:00:15 -0700

    Updated Dungeon.java class.

--- DungeonAdventure/src/Model/Dungeon.java
+++ DungeonAdventure/src/Model/Dungeon.java
@@ -1,4 +1,51 @@
 package Model;
 
+import java.util.Arrays;
+
 public class Dungeon {
+    private Room[][] myDungeon;
+    private int myAdventurerLocation;
+
+    public Dungeon() {
+    }
+
+    private boolean isTransversable(Room[][] theRoom){
+        return false;
+    }
+
+    public Room[][] getMyDungeon() {
+        return myDungeon;
+    }
+
+    public void setMyDungeon(final Room[][] theDungeon) {
+        myDungeon = theDungeon;
+    }
+
+    public int getMyAdventurerLocation() {
+        return myAdventurerLocation;
+    }
+
+    public void setMyAdventurerLocation(final int theAdventurerLocation) {
+        myAdventurerLocation = theAdventurerLocation;
+    }
+
+    private void placeExit(){
+
+    }
+
+    private void placeEntrance(){
+
+    }
+
+    private void placePillars(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Dungeon{" +
+                "myDungeon=" + Arrays.toString(myDungeon) +
+                ", myAdventurerLocation=" + myAdventurerLocation +
+                '}';
+    }
 }
