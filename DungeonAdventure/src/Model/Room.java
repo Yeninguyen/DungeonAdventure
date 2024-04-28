83b9388c0b71c2fd09f18a5b10ec326f02f2a1cf
Faisal Nur <102789269+Faisalkn@users.noreply.github.com>
2024-04-27 18:48:44 -0700

    Updated the room class.

--- DungeonAdventure/src/Model/Room.java
+++ DungeonAdventure/src/Model/Room.java
@@ -1,4 +1,59 @@
 package Model;
 
 public class Room {
+    private int myHealingPotion;
+    private int myPit;
+    private boolean myEntrance;
+    private String myPillar;
+    private boolean myVisionPotion;
+
+
+    public Room(final int theHealingPotion, final int thePit, final boolean theEntrance,
+                String thePillar, final boolean theVisionPotion) {
+       myHealingPotion = theHealingPotion;
+       myPit = thePit;
+       myEntrance = theEntrance;
+       myPillar = thePillar;
+       myVisionPotion = theVisionPotion;
+    }
+
+    public int getMyHealingPotion() {
+        return myHealingPotion;
+    }
+
+    public void setMyHealingPotion(final int theHealingPotion) {
+        myHealingPotion = theHealingPotion;
+    }
+
+    public int getMyPit() {
+        return myPit;
+    }
+
+    public void setMyPit(final int thePit) {
+        myPit = thePit;
+    }
+
+    public boolean isMyEntrance() {
+        return myEntrance;
+    }
+
+    public void setMyEntrance(final boolean theEntrance) {
+        myEntrance = theEntrance;
+    }
+
+    public String getMyPillar() {
+        return myPillar;
+    }
+
+    public void setMyPillar(final String thePillar) {
+        myPillar = thePillar;
+    }
+
+    public boolean isMyVisionPotion() {
+        return myVisionPotion;
+    }
+
+    public void setMyVisionPotion(final boolean theVisionPotion) {
+        myVisionPotion = theVisionPotion;
+    }
 }
