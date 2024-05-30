ce33e3c510a7d19683f86d3804126ae54a4318bc
Yeninguyen <nhipnguyen0805@gmail.com>
2024-05-29 22:03:32 -0700

    Update SuperItems, Items, DungeonPanel and images for the objects

---
+++ DungeonAdventure/src/Controller/SuperItems.java
@@ -0,0 +1,18 @@
+package Controller;
+
+import View.DungeonPanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+public class Objects {
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int worldX, worldY;
+
+    public void draw(Graphics2D theGraphics2D, DungeonPanel theDungeonPanel) {
+        
+    }
+}
